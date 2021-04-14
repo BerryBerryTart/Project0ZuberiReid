b554d56a9e574f2e6119e044cb6de095c3b244bc
Berry <windbird44@gmail.com>
2021-04-14 09:51:35 -0400

    please stop tracking folders

--- src/main/java/com/berry/app/Application.java
+++ src/main/java/com/berry/app/Application.java
@@ -1,10 +1,31 @@
 package com.berry.app;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.javalin.Javalin;
+
 public class Application {
 
+	private static Javalin app;
+	private static Logger logger = LoggerFactory.getLogger(Application.class);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		app = Javalin.create();
+		
+		app.error(404, ctx ->{
+			Map<String, String> errorMap = new HashMap<String, String>();
+			errorMap.put("Error", "Path Not Found");
+			ctx.json(errorMap);
+			
+			String URI = ctx.req.getRequestURI();
+			String httpMethod = ctx.req.getMethod();
+			logger.info(httpMethod + " request to unknown endpoint '" + URI + "'");
+		});
+		
+		app.start(5000);	
 	}
-
 }
